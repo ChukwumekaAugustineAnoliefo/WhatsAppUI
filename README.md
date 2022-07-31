843a14bcf37a142f7f9e3c9fa4f058e7a479ab5b
ChukwumekaAugustineAnoliefo <chuksanoliefo@gmail.com>
2022-07-31 13:47:06 +0100

    last changes made

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# Whatsapp UI
+
+This a completely Responsive Whatsapp UI App that works on Android, iOS, Web & Desktop! Works as WhatsApp App on Smaller Screen Sizes and WhatsApp Web on Bigger Screen Sizes!
+
+
+
+
+
+## YouTube
+this project was created following a youtube tutorial by [Rivaan Ranawat](https://www.youtube.com/watch?v=g1hmeP8WZHU) 
+from this tutorial i learnt how to implement responsive design for different layout screens how to create custom reusable widgets/constants or generics such as the colors.dart used here.
+
+
+
+
+    
+
+
---
+++ lib/info.dart
@@ -0,0 +1,118 @@
+const info = [
+  {
+    'name': 'Samuel Ngaji',
+    'message': 'Guy how far',
+    'time': '2:53 am',
+    'profilePic':
+        'https://img.freepik.com/free-photo/cute-business-woman-idea-thinking-present-pink-background-3d-rendering_56104-1460.jpg?w=1380&t=st=1659269634~exp=1659270234~hmac=26dddfc604d610fe967df2e9d6a8586ba4c7fcc3d6ceb5696e9c90f7fde3ceef',
+  },
+  {
+    'name': 'My Diamond',
+    'message': 'Baby I Missed you',
+    'time': '12:30 am',
+    'profilePic':
+        'https://img.freepik.com/free-photo/travel-3d-render-logo-illustration_460848-11289.jpg?w=900&t=st=1659269804~exp=1659270404~hmac=08481ee72d2a052c1b0c5f133a6f59e0b7bbcdd74991ea9adb1031cf56ee13cd',
+  },
+  {
+    'name': 'Moses',
+    'message': 'You still loved your ex?.',
+    'time': '12:03 pm',
+    'profilePic':
+        'https://img.freepik.com/premium-photo/hipster-head-with-empty-space-3d-render-illustration_1172-992.jpg?w=1380',
+  },
+  {
+    'name': 'Kayode',
+    'message': 'Idiot, i need my money',
+    'time': '2:16 am',
+    'profilePic':
+        'https://img.freepik.com/premium-photo/men-are-listening-music-with-headphones-comfortably-happily3d-render_544235-322.jpg?w=826',
+  },
+  {
+    'name': 'Mom',
+    'message': 'Happy birth day Mama Emeka',
+    'time': '10:00 am',
+    'profilePic':
+        'https://img.freepik.com/premium-photo/futuristic-abstract-portrait-neon-colors-digital-3d-figurative-illustration-with-glowing-fluid-shapes_348452-70.jpg?w=826',
+  },
+  {
+    'name': 'Dad',
+    'message': 'Yo!!!!! Long time, no see!?',
+    'time': '12:53 am',
+    'profilePic':
+        'https://img.freepik.com/premium-photo/futuristic-abstract-portrait-neon-colors-digital-3d-figurative-illustration-with-glowing-fluid-shapes_348452-77.jpg?w=826',
+  },
+  {
+    'name': 'Mr Ekpeyong Iyang',
+    'message': 'Sir, U don chop?',
+    'time': '12:25 pm',
+    'profilePic':
+        'https://img.freepik.com/premium-photo/cute-little-girl-holding-tablet-computer-her-hands-games-children-use-technology_686385-1247.jpg?w=1380',
+  },
+  {
+    'name': 'Side Chick',
+    'message': 'Common Baby',
+    'time': '4:09 am',
+    'profilePic':
+        'https://img.freepik.com/premium-photo/vr-glasses-events-metaverse-party-media-player-headset-entertainment-party-metaverse-illustrations_357568-3973.jpg?w=1800',
+  },
+  {
+    'name': 'Emma Cisco',
+    'message': 'Bro, when are you getting married',
+    'time': '8:56 am',
+    'profilePic':
+        'https://img.freepik.com/premium-photo/vr-glasses-events-metaverse-party-media-player-headset-entertainment-party-metaverse-illustrations_357568-3974.jpg?w=1800',
+  },
+  {
+    'name': 'Emma 3D',
+    'message': 'Do you have any anatomy pdf',
+    'time': '11:00 am',
+    'profilePic':
+        'https://img.freepik.com/premium-photo/nft-sandbox-nftprojects-avatars-metaverse-nft-art-gallery-3d-illustrations_357568-3933.jpg?w=1800',
+  },
+];
+
+const messages = [
+  {"isMe": true, "text": "Guy whats happening!!", "time": "10:00 am"},
+  {"isMe": false, "text": "I m fine but sapa?", "time": "11:00 am"},
+  {
+    "isMe": true,
+    "text": "oooh sapa na normal thing bro enjoy!",
+    "time": "11:01 am"
+  },
+  {"isMe": false, "text": "you mean its normal.", "time": "11:01 am"},
+  {"isMe": true, "text": "yea very normal bro"},
+  {"isMe": false, "text": "Send me 2k na ^^", "time": "11:04 am"},
+  {"isMe": true, "text": " for what?", "time": "11:05 am"},
+  {
+    "isMe": false,
+    "text": "to chop na",
+    "time": "11:06 am",
+  },
+  {
+    "isMe": true,
+    "text": "I no sabi my transfer pin again!",
+    "time": "11:15 am",
+  },
+  {"isMe": false, "text": "u see your life!", "time": "11:17 am"},
+  {"isMe": false, "text": "you no fit ever help person", "time": "11:16 am"},
+  {
+    "isMe": true,
+    "text": "you they use your own money chase women!",
+    "time": "11:17 am"
+  },
+  {
+    "isMe": false,
+    "text": "Cool, did you like the content?",
+    "time": "11:18 am",
+  },
+  {
+    "isMe": true,
+    "text": "I loved it?",
+    "time": "11:19 am",
+  },
+  {
+    "isMe": false,
+    "text": "OMG! Woah! Thanks!",
+    "time": "11:20 am",
+  },
+];
